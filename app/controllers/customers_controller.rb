a126aad50c8ffb6d0b3fc76133ea4a4bb5cd65fe
beccaelenzil <beccaelenzil@gmail.com>
2019-11-27 10:48:25 -0800

    up to date

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,8 @@
+KEYS = [:name, :registered_at, :address, :city, :state, :postal_code, :phone, :id]
+
 class CustomersController < ApplicationController
   def index
-    customers = Customer.all.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone])
+    customers = Customer.all.as_json(only: KEYS)
     render json: customers, status: :ok
   end
 end
